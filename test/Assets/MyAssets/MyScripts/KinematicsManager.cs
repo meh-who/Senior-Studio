c4c97db2a32562f7e514c60f9769a5c5c2f511a8
unknown <62934421+meh-who@users.noreply.github.com>
2021-04-07 14:41:30 -0400

    put on cam & throw brain

---
+++ test/Assets/MyAssets/MyScripts/KinematicsManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicsManager : MonoBehaviour
+{
+    private bool brainOn = true;
+    //Moves this GameObject 2 units a second in the forward direction
+    void Update()
+    {
+        
+    }
+
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        //print(other.name);
+        if (other.name == "Camera"){
+            Rigidbody CamRb = other.GetComponent<Rigidbody>();
+            CamRb.isKinematic = true;
+            CamRb.useGravity = false;
+            print("I put the camera on!");
+        }
+
+    }
+
+    // private void OnTriggerStay(Collider other)
+    // {   
+    //     print(other.name);
+    //     if (other.name == "Brain"){
+    //         brainOn = true;
+    //     }else{
+    //         brainOn = false;
+    //     }
+    // }
+
+    // void onTriggerEnter(Collider other){
+    //     print(other.name);
+    //     if (other.name == "Camera"){
+    //         print("cam");
+    //         Rigidbody CamRb = other.GetComponent<Rigidbody>();
+    //         CamRb.isKinematic = true;
+    //         CamRb.useGravity = false;
+    //     }
+    // }
+}
